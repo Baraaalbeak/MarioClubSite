706699857f66371dedbe5cc94524e88758eb1105
Baraaalbeak <90406266+Baraaalbeak@users.noreply.github.com>
2021-09-26 12:42:22 +0300

    first upload

---
+++ main.js
@@ -0,0 +1,8 @@
+// document.addEventListener("DOMContentLoaded", function(){
+//     const NBResponsive = document.getElementsByClassName("NBResponsive")[0];
+//     const NBLinks = document.getElementsByClassName("NBLinks")[0];
+    
+//     NBResponsive.addEventListener('click',() => {
+//         NBLinks.classList.toggle('active')
+//     });
+// });
